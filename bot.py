762db6a5bb1e620861e28b602bfafafdf3bc5104
Gennadii <brundrom@gmail.com>
2023-02-27 22:12:53 +0300

    Added functions for check all SN and other.

--- bot.py
+++ bot.py
@@ -3,22 +3,23 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
-logging.basicConfig(level=logging.INFO)
-
 TOKEN = "6001520005:AAFYFbOvhZAfeuwELoYE2WGiNQGWqA3Eh1E"
-MSG_GOOD = "Yes, this SN on the Igor. Checked!"
-MSG_BAD = "No, this SN is not Igor. Fixed."
+MSG_GOOD = "\U00002705 Yes, this SN in the base. Checked!"
+MSG_BAD = "\U0000274C No, this SN is not in the base. Fixed."
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
-serialNum = "2"
+
+f = open("baseserials.td", "r")
+serialsN = f.read()
+f.close()
 
 
 @dp.message_handler(commands=['start'])
 async def start(message):
 
     # user_full_name = message.from_user.full_name
-    await message.reply("Hello, I am your bot helper")
+    await message.reply("Hello, I am your bot. Opt: /start /allsn /serials /clean")
 
 
 @dp.message_handler(commands=['clean'])
@@ -29,16 +30,33 @@ async def start(message):
     await message.reply("All SN in file was cleared!")
 
 
+@dp.message_handler(commands=['allsn'])
+async def start(message):
+    f = open("all_sn.td", "r")
+    text = f.read()
+    f.close()
+    await message.reply(text)
+
+
+@dp.message_handler(commands=['serials'])
+async def start(message):
+    await message.reply(serialsN)
+
+
 @dp.message_handler(content_types=['text'])
 async def get_text_messages(message):
     user_id = message.from_user.id
-    if message.text == '2345':
+    if message.text in serialsN:
         await bot.send_message(user_id, MSG_GOOD)
+        f = open("all_sn.td", "a")
+        f.write("*** " + message.text + " ***\n")
+        f.close()
     else:
         await bot.send_message(user_id, MSG_BAD)
         f = open("all_sn.td", "a")
-        f.write(message.text)
+        f.write(message.text + "\n")
         f.close()
+
 
 if __name__ == '__main__':
     executor.start_polling(dp)
